271b87184d8add96ff05307ade78bb0d47860b39
sandhi-artha <sandiartha99@yahoo.co.id>
2020-05-27 17:51:30 +0200

    added predict route for clarifai

--- server.js
+++ server.js
@@ -1,8 +1,26 @@
 const express = require('express');
+require('dotenv').config();
+const Clarifai = require('clarifai');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
 
 const app = express();
+
+const clarifaiApp = new Clarifai.App({ apiKey: process.env.API_KEY });
+
+// middlewares
+app.use(bodyParser.json())
+app.use(cors());        // resolves Access-Control-Allow-Origin error
+
 app.get('/', (req,res) => {
     res.json('sending from the server');
+})
+
+app.post('/predict', (req,res) => {
+    clarifaiApp.models.predict(Clarifai.DEMOGRAPHICS_MODEL, req.body.url)
+    .then(data => res.json(data))
+    .catch(err => res.json("Can't fetch API data"));
 })
 
 app.listen(5000, () => {

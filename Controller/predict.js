b890ea1d20dbc07bb706bfdd2ef7d13f2225b2f4
sandhi-artha <sandiartha99@yahoo.co.id>
2020-05-27 22:48:28 +0200

    add db to store predictions

--- Controller/predict.js
+++ Controller/predict.js
@@ -2,10 +2,11 @@ const Clarifai = require('clarifai');
 // const clarifaiApp = new Clarifai.App({ apiKey: process.env.API_KEY });
 
 // get simulated data
+// https://samples.clarifai.com/face-det.jpg
 const mockup = require('./mockup');
 
-const getPrediction = (response) => {
-    return response.outputs[0].data.regions.map(region => {
+const getPrediction = (response, db) => {
+    const data = response.outputs[0].data.regions.map(region => {
         const {top_row, left_col, bottom_row, right_col} = region.region_info.bounding_box;
         // calculate bounding box overlayed in image
         const box = { bTop: top_row * 100, bLeft: left_col * 100, bBot: 100 - (bottom_row * 100), bRight: 100 - (right_col * 100) };
@@ -15,27 +16,48 @@ const getPrediction = (response) => {
         const race = region.data.concepts[22].name;
         return { box, age, gender, race }               // returns box:box, age:age etc as object
     })
+    savePredict(db, data);
+    return data
 }
 
-const handlePredict = (req, res) => {
+const savePredict = (db, data) => {
+    const insertData = data.map((pred,i) => {
+        return {
+            imgid: i,
+            age: Number(pred.age),
+            gender: pred.gender === 'masculine',
+            race: i,
+            btop: pred.box.bTop,
+            bleft: pred.box.bLeft,
+            bbot: pred.box.bBot,
+            bright: pred.box.bRight
+        }
+    })
+    db('predictions').insert(insertData)
+    .then(console.log("data stored"))
+    .catch(err => res.status(400).json("database error"))
+}
+
+
+const handlePredict = (req, res, db) => {
     // clarifaiApp.models.predict(Clarifai.DEMOGRAPHICS_MODEL, req.body.url)
     // .then(response => res.json(getPrediction(response)))
     // .catch(err => res.json("Can't fetch API data"));
     switch (req.body.url) {
         case "https://samples.clarifai.com/face-det.jpg":
             console.log("url1");
-            res.json(getPrediction(mockup.url1));
+            res.json(getPrediction(mockup.url1, db));
             break;
         case "https://cms-tc.pbskids.org/parents/_pbsKidsForParentsHero/homeschool-socialization.jpg?mtime=20190423144706":
             console.log("url2");    // no CORS header
-            res.json(getPrediction(mockup.url2));
+            res.json(getPrediction(mockup.url2, db));
             break;
         case "https://cdn.elearningindustry.com/wp-content/uploads/2018/12/is-socializing-in-the-workplace-important-for-team-productivity-1024x574.jpg":
             console.log("url3");
-            res.json(getPrediction(mockup.url3));
+            res.json(getPrediction(mockup.url3, db));
             break;
         default:
-            res.json(getPrediction(mockup.url1));
+            res.json(getPrediction(mockup.url2, db));
             break;
     }
 }
--- server.js
+++ server.js
@@ -1,7 +1,22 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const knex = require('knex');
+
+
 
 const app = express();
+
+// database config
+const db = knex({
+    client: 'pg',               // change to what db ur using
+    connection: {
+        host : '127.0.0.1',     // location of db (hosted platform)
+        user : 'postgres',
+        password : 'sandhi',
+        database : 'facedetection'
+    }
+});
+
 
 // env variables
 require('dotenv').config();
@@ -20,12 +35,15 @@ const predict = require('./Controller/predict');
 // End Points
 app.get('/', (req,res) => { res.json('sending from the server') })
 
-app.post('/predict', (req,res) => predict.handlePredict(req, res))
+app.post('/predict', (req,res) => predict.handlePredict(req, res, db))
 
 app.listen(PORT, () => { console.log('app is running on port ' + PORT) })
 
 /* TODO
     predict route
-    1. process the prediction data, and store in database
-    2. send only box, age, gender, race
+    1. process the prediction data, and store in database       DONE
+    2. send only box, age, gender, race     DONE
+    3. create mockup data   DONE
+    4. create a database according to scheme    DONE
+    5. simulate storing prediction into database
 */

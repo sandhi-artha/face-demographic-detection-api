500804dcc20a177cd2acb17b950f54a2eb36bcf8
sandhi-artha <sandiartha99@yahoo.co.id>
2020-06-01 09:38:35 +0200

    fixed returned data from register route

--- Controller/register.js
+++ Controller/register.js
@@ -3,13 +3,10 @@ const bcrypt = require('bcryptjs');
 const handleRegister = (req, res, db) => {
     const { name, email, password } = req.body;
     let hash = bcrypt.hashSync(password, 10);       // encrypt the password and store as hash
-    db('users').returning(['name','email','joined']).insert({
-        name: name,
-        email: email,
-        joined: new Date(),
-        hash: hash
-    }).then(data => {
-        console.log(data.name+" registered!");
+    db('users').insert({ name, email, joined: new Date(), hash })
+    .then(data => {
+        delete data[0].hash;                                // don't want the hash sent to the front end
+        console.log(data[0].name+" registered!");
         res.json({profile: data[0], images: [], predictions: []});
     })
     .catch(err => res.status(400).json('Error: Name or email has already been used!'))
